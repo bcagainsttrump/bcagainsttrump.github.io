06b8b2219e2396ce80dab33f6be7ef2e98bd1077
unilogue <unilogue.io@gmail.com>
2017-01-24 00:07:54 -0500

    added mobile queries

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -2,7 +2,7 @@ $(function() {
 
 $(document).ready(function() {
 
-  1px = (100 / document.documentElement.clientWidth)vw
+/*  1px = (100 / document.documentElement.clientWidth)vw */
 
    });
  });
