4405d62873ec030ab75e05011198b8da179aa776
unilogue <unilogue.io@gmail.com>
2017-01-24 05:16:42 -0500

    new mobilw + nav links

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,31 @@ $(function() {
 
 $(document).ready(function() {
 
-/*  1px = (100 / document.documentElement.clientWidth)vw */
+  /*
+
+  if (top.location.pathname === '/links/') {
+    $('#links').attr('id', 'hi');
+  } else if (top.location.pathname === '/events/') {
+    $('#events').attr('id', 'hi');
+  } else if (top.location.pathname === '/petitions/') {
+    $('#petitions').attr('id', 'hi');
+  } else if (top.location.pathname === '/organize/') {
+    $('#organize').attr('id', 'hi');
+  }
+
+  */
+
+var mq = window.matchMedia( "(max-device-width : 420px)" );
+
+if (mq.matches) {
+
+  $('#cu').attr('id', 'hi');
+  $('.nav').hide();
+  $('#dropDown').show();
+} else {
+  $('.nav').show();
+  $('#dropDown').hide();
+}
 
    });
  });
