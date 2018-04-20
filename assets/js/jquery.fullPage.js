de05c4a887dd95ae4b73aeeeb5fe522c99a53b8d
unilogue <simsluva@gmail.com>
2018-04-20 18:19:28 -0400

    reset true

--- assets/js/jquery.fullPage.js
+++ assets/js/jquery.fullPage.js
@@ -134,7 +134,7 @@
             fadingEffect: false,
             normalScrollElements: null,
             scrollOverflow: true,
-            scrollOverflowReset: false,
+            scrollOverflowReset: true,
             scrollOverflowHandler: $.fn.fp_scrolloverflow ? $.fn.fp_scrolloverflow.iscrollHandler : null,
             scrollOverflowOptions: null,
             touchSensitivity: 5,
